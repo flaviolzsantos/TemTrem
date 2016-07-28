90f99e937272db56cb29cd6d5abd6ee8e59d63a6
flaviolzsantos <flaviolzsantos@gmail.com>
2016-07-28 20:56:51 -0300

    inicio email

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 var request = require("request");
 var express = require("express");
+var email = require("nodemailer");
 var app = express();
+var transporter = email.createTransport('smtps://flaviolzsantos%40gmail.com:pass@smtp.gmail.com');
+
 
 app.get("/trem",function(req, res){
 	temtrem(res);
 	});
 
 app.listen(3000);
+
+function enviaEmail(){
+	var mailOptions = {
+		    from: '"aviso" <@blurdybloop.com>', // sender address
+		        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+			    subject: 'Hello ✔', // Subject line
+			        text: 'Hello world 🐴', // plaintext body
+				    html: '<b>Hello world 🐴</b>' // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+		    if(error){
+			            return console.log(error);
+				        }
+					    console.log('Message sent: ' + info.response);
+	});
+	}
 
 function temtrem(res){
 request({
